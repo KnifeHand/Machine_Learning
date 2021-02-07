c24d1fd091117919d35049aa79d9bd6358b0f488
Matt Hurt <mhurt@msudenver.edu>
2021-02-07 11:22:14 -0700

    Does not compile due to line 63 giving an error that 'o-' is the issue for not being able to convert into a float.

--- Homework1/Homework1_Matt_Hurt.py
+++ Homework1/Homework1_Matt_Hurt.py
@@ -5,85 +5,62 @@
 Linear Regression
 
 """
-from audioop import bias
-
 import numpy as np
-import numpy as py
-import inline as inline
 import matplotlib.pyplot as plt
-import pandas as pd
 
-# H-y=L  Loss is a function of w and b
 # Generate samples
-x_data = np.array([35, 38, 31, 20, 22, 17, 60, 8, 60])
-y_data = 2 * x_data + 50 + 5 * np.random.random(100)
+x_data = np.array([35.,38.,31.,20.,22.,17.,60.,8.,60.])
+y_data = 2*x_data+50+5*np.random.random()
 
-bb = np.arrange(0, 100, 1)  # bias
-ww = np.arrange(-5, 5, 0.1)  # weight
-Z = np.zeros((len(bb), len(ww)))
+# Plot the landscape
+bb = np.arange(0,100,1)# bias
+ww = np.arange(-5,5,0.1)# weight
+Z = np.zeros((len(bb),len(ww)))
+
+# # # For possible w and b values
+# bias = np.arange(0, 100, 1)
+# weight = np.arange(-5, 5, 0.1)
+# Z = np.zeros((len(bias), len(weight)))
 
 for i in range(len(bb)):
     for j in range(len(ww)):
         b = bb[i]
         w = ww[j]
-        Z[i][j] = 0  # loss function
+        Z[j][i] = 0  # loss function
         for n in range(len(x_data)):
-            Z[j][i] = 10
-            Z[j][i] = 1
-
-# # Generate data samples
-# x_data = np([35., 38., 31., 20., 25., 17., 60., 8., 60.])
-# y_data = 2 * x_data + 50 + 5 * np.random.random(10)
-
-# # For possible w and b values
-bias = np.arange(0, 100, 1)
-weight = np.arange(-5, 5, 0.1)
-Z = np.zeros((len(bias), len(weight)))
-# TODO: ...........?
-
-# The contour figure of the loss
-for i in range(len(bias)):
-    for j in range(len(weight)):
-        b = bias[i]
-        w = weight[j]
-        Z[j][i] = 0  # Loss calculation
-        for n in range(len(x_data)):  # TODO: define x_data and y_data
-            Z[j][i] = Z[j][i] + (w * x_data[n] + b - y_data[n]) ** 2  # TODO
+            Z[j][i] = Z[j][i] + (w*x_data[n]+b - y_data[n])**2 # this is the loss
+            Z[j][i] = Z[j][i]/len(x_data)
 
 # Find best w and b
 b = 0  # initial b
 w = 0  # initial w
-lr = 0.0001  # learning rate
-iteration = 15000  # example iteration number
+lr = 0.001  # learning rate
+iteration = 10000  # example iteration number
+
+# Iterations
 b_history = [b]
 w_history = [w]
 
-# model by gradient descent
-# .... TODO
-
-b_history.append(b)
-w_history.append(w)
-# ....TODO
-
-plt.plot(b_history, w_history, 'o-', ms=3, lw=1.5, color='black')
+# The contour figure of the loss
 for i in range(iteration):
     b_grad = 0.0
     w_grad = 0.0
     for n in range(len(x_data)):
-        b_grad = 0  # TODO: .............?
-        w_grad = 0  # TODO: .............?
+        b_grad += b_grad+(b+w*x_data[n]-y_data[n])*1.0
+        w_grad += w_grad+(b+w*x_data[n]-y_data[n])*x_data[n]
+    b = b-b_grad*lr
+    w = w-w_grad*lr
 
-# Update parameters
-# b #TODO ............?
-# w #TODO ............?
+# model by gradient descent
 
 # Store parameters for plotting
 b_history.append(b)
 w_history.append(w)
 
-plt.xlim(0, 100)
-plt.ylim(-5, 5)
-plt.contour(bias, weight, Z, 50, alpha=0.5, cmap=plt.get_cmap('jet'))
+plt.xlim(0,100)
+plt.ylim(-5,5)
+plt.contour(bb, ww, Z, 50, alpha=0.5, cmap=plt.get_cmap('jet'))
+plt.contour(b_history, w_history, 'o-', ms=3, lw=1.5, color='black')
 plt.show()
 
 ###############################################################################
@@ -135,6 +112,4 @@ plt.show()
 # plt.legend()
 # plt.show()
 
-"""
 
-"""
