1a9d3d21f4a662a68b7e3f2acebac811875c2082
Matt Hurt <mhurt@msudenver.edu>
2021-02-07 12:40:51 -0700

    Success!  Plot window shows up with the some of the components for the gradient.  Commenting out the plt.contour function to draw the line fixes the problem however, the idea for drwaing the line doesn't work...?

--- Homework1/Homework1_Matt_Hurt.py
+++ Homework1/Homework1_Matt_Hurt.py
@@ -29,7 +29,7 @@ for i in range(len(bb)):
         Z[j][i] = 0  # loss function
         for n in range(len(x_data)):
             Z[j][i] = Z[j][i] + (w * x_data[n] + b - y_data[n]) ** 2  # this is the loss
-            Z[j][i] = Z[j][i] / len(x_data)
+        Z[j][i] = Z[j][i] / len(x_data)
 
 #plt.contour(bb, ww, Z, 50, alpha=0.5, cmap=plt.get_cmap('jet'))
 
@@ -48,22 +48,24 @@ for i in range(iteration):
     b_grad = 0.0
     w_grad = 0.0
     for n in range(len(x_data)):
-        b_grad = b_grad+(b + w * x_data[n] - y_data[n]) * 1.0
-        w_grad = w_grad+(b + w * x_data[n] - y_data[n]) * x_data[n]
+        b_grad += b_grad+(b + w * x_data[n] - y_data[n]) * 1.0
+        w_grad += w_grad+(b + w * x_data[n] - y_data[n]) * x_data[n]
+
     b = b - b_grad * lr
     w = w - w_grad * lr
+
     # Store parameters for plotting
     b_history.append(b)
     w_history.append(w)
 
 # model by gradient descent
 
-
-plt.contour(bb, ww, Z, 50, alpha=0.5, cmap=plt.get_cmap('jet'))
-plt.plot([50], [2], 'x', ms=12, markeredgewidth=3, color='orange')
-plt.contour(b_history, w_history, 'o-', ms=3, lw=1.5, color='black')
 plt.xlim(0, 100)
 plt.ylim(-5, 5)
+plt.contour(bb, ww, Z, 50, alpha=0.5, cmap=plt.get_cmap('jet'))
+plt.plot([50], [2], 'x', ms=12, markeredgewidth=3, color='orange')
+#plt.contour(b_history, w_history, 'o-', ms=3, lw=1.5, color='black')
+
 plt.xlabel(r'$b$',fontsize=16)
 plt.ylabel(r'$w$',fontsize=16)
 plt.show()
